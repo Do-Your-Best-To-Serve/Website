d74aebdbf487204ab96fb53fadeeaf886c31bbb0
AD <liyoujun600@gmail.com>
2022-12-07 19:10:03 +0800

    增加dashboard

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+
+from discord_oauth import DiscordOauth
 
 app = Flask(__name__, static_url_path='/cdn')
 
@@ -28,5 +30,34 @@ def invited():
     return render_template('invited.html')
 
 
+# Route for index page
+# Provides user login capabilities
+@app.route('/login', methods=['GET'])
+def login():
+    return redirect(DiscordOauth.login_url)
+
+
+# Route for dashboard
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    code = request.args.get('code')
+    access_token = DiscordOauth.get_access_token(code)
+    print(access_token)
+    if access_token:
+
+        user_object = DiscordOauth.get_user(access_token)
+        user_guild_object = DiscordOauth.get_user_current_guild(access_token)
+
+        id, avatar, username, usertag = user_object.get('id'), user_object.get('avatar'), user_object.get('username'), \
+                                        user_object.get('discriminator')
+
+        return render_template('dashboard.html',
+                               render_user_avatar=f'https://cdn.discordapp.com/avatars/{id}/{avatar}.png',
+                               render_username=f'{username}#{usertag}', render_guild=user_guild_object)
+    else:
+        return redirect('login')
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host='192.168.1.73', port=8000)
+    # app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=False, host='192.168.1.73', port=8000)
